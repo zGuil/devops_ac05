56a20dde5c19c73c418b35a85c30cffd89644bec
zGuil <54157470+zGuil@users.noreply.github.com>
2019-09-26 21:44:15 -0300

    Update test_conta_corrente.py

--- com/mongoloides/test/test_conta_corrente.py
+++ com/mongoloides/test/test_conta_corrente.py
@@ -1,7 +1,9 @@
+import pytest
 from com.mongoloides.conta_corrente import ContaCorrente
-
-conta = ContaCorrente(1802, "Wesley", saldo=10)
 
 
 def test_alterarNome():
-    assert conta.alterarNome("Guilherme") == self.nomeCorrentista
+    conta = ContaCorrente(1802, "Wesley", saldo=10)
+    assert conta.alterarNome("Guilherme") == conta.nomeCorrentista
+    assert conta.deposito(300) == conta.saldo
+    assert conta.saque(300) == conta.saldo

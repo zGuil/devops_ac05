c16e66833008889e7e304843f0f364ea3bb27647
zGuil <54157470+zGuil@users.noreply.github.com>
2019-11-29 14:36:19 -0300

    Update calcula_parcela.py

--- com/mongoloides/calcula_parcela.py
+++ com/mongoloides/calcula_parcela.py
@@ -5,6 +5,6 @@ def valorPagamento(valor, diasAtraso):
     if (diasAtraso > 0):
         multa = valor * 0.03
         adicionalAtraso = valor * (diasAtraso * 0.01)
-        return valor + multa + adicionalAtraso
+        return ""
     else:
         return valor
